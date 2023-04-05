4b2a7b272feb652603cff5d3b3f7ff71b5a6a9d1
tylerchessa <tylerchessa@gmail.com>
2023-04-05 16:34:36 +0000

    add user login/logout feature using bcrypt

--- config/routes.rb
+++ config/routes.rb
@@ -21,6 +21,25 @@ Rails.application.routes.draw do
   end
 
 
+	# Add a root route if you don't have one...
+	# We can use users#new for now, or replace this with the controller and action you want to be the site root:
+	# root to: 'users#new'
+  	
+  # sign up page with form:
+get 'users/new' => 'users#new', as: :new_user
+
+# create (post) action for when sign up form is submitted:
+post 'users' => 'users#create'
+
+# log in page with form:
+get '/login'     => 'sessions#new'
+	
+# create (post) action for when log in form is submitted:
+post '/login'    => 'sessions#create'
+
+# delete action to log out:
+delete '/logout' => 'sessions#destroy'  
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
