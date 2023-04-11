8bb6b0eea6dbdaac07fbc81f459f35b6c084018c
tylerchessa <tylerchessa@gmail.com>
2023-04-11 21:21:38 +0000

    implement home page testing

---
+++ cypress/integration/home.spec.js
@@ -0,0 +1,15 @@
+{/* <reference types="cypress" /> */}
+
+describe('load the homepage', () => {
+  beforeEach(() => {
+    cy.visit('/')
+  })
+
+  it("There is products on the page", () => {
+    cy.get(".products article").should("be.visible");
+  });
+
+  it("There is 2 products on the page", () => {
+    cy.get(".products article").should("have.length", 2);
+  });
+})
